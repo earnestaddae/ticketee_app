6017baba1be2a2c54532e1f3d49a72d57689d1cd
Ernest Addae <earnestaddae@gmail.com>
2021-03-02 21:38:59 +0800

    Add dependency on project deletion

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
-  has_many :tickets
+  # has_many :tickets, dependent: :destroy # with a callback
+  has_many :tickets, dependent: :delete_all # deletes all associated tickets to a project w/o callbacks
   validates :name, presence: true
 end
