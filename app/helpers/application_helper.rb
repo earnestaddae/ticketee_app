9222aad8c06c4ecb30e9eeec2b66f206fa5e9978
Ernest Addae <earnestaddae@gmail.com>
2021-03-16 09:24:51 +0800

    Hide links from inappropriate users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,16 @@
 module ApplicationHelper
+  # generates dynamic page title
   def title(*parts)
     unless parts.empty?
       content_for :title do
         (parts << "TicketeeApp").join(" - ")
       end
     end
+  end
+
+  # admins only block
+
+  def admins_only(&block)
+    block.call if current_user.try(:admin?)
   end
 end
