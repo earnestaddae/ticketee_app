f1eb334e9df5d33aa81f58524edd14b4887d562b
Ernest Addae <earnestaddae@gmail.com>
2021-03-17 15:56:14 +0800

    Allow admins to default a state

--- app/models/state.rb
+++ app/models/state.rb
@@ -3,4 +3,10 @@ class State < ApplicationRecord
   def to_s
     name
   end
+
+  # state default maker
+  def make_default!
+    State.update_all(default: false)
+    update!(default: true)
+  end
 end
