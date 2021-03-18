51eb64bccc4e9ea91bd5046bc5677630a25ed72e
Ernest Addae <earnestaddae@gmail.com>
2021-03-18 15:29:40 +0800

    Create join_table between tickets and users for habtm association

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,6 +5,9 @@ class Ticket < ApplicationRecord
   # has_one_attached :attachment
   has_many_attached :attachments
   has_many :comments, dependent: :destroy
+  has_and_belongs_to_many :watchers,
+                          :join_table => "ticket_watchers",
+                          :class_name => "User"
 
   before_create :assign_default_state
 
